34a5be7c77e0f09d4ab0723330b9834dbb774c27
hungnv1997 <46401220+hungnv1997@users.noreply.github.com>
2023-03-09 08:30:38 +0000

    feat(index): add funcs implement session storage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
 import { isEmpty,isEmptyObject,formatNumberToDecimal } from './checkFormat/format'
 import { mapToHankaku } from './jaLang/jaLang'
 export { isEmpty,isEmptyObject,formatNumberToDecimal,mapToHankaku }
-export const sum = (a: number, b: number) => a + b
+/**
+ *
+ * @param {String} name
+ * @param {String} data
+ */
+export function setSessionStorageByName(name:string, data:any) {
+    window.sessionStorage.setItem(name, JSON.stringify(data));
+  }
+  
+  /**
+   *
+   * @param {String} name
+   */
+  export function removeSessionStorageByName(name:string) {
+    window.sessionStorage.removeItem(name);
+  }
+  
+  /**
+   *
+   * @param {String} name
+   * @returns
+   */
+  export function getSessionStorageByName(name:string) {
+    const dataSessionStorage:any = window.sessionStorage.getItem(name);
+    return isEmpty(dataSessionStorage) ? null : JSON.parse(dataSessionStorage);
+  }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,41 @@
-import { sum } from '../src'
+import { setSessionStorageByName,
+  removeSessionStorageByName,
+  getSessionStorageByName, } from '../src'
 
-test('sum', () => {
-  expect(sum(1, 2)).toEqual(3)
-})
+  describe('SessionStorage functions', () => {
+    beforeEach(() => {
+      // Clear session storage before each test
+      window.sessionStorage.clear();
+    });
+  
+    it('setSessionStorageByName should store data in session storage', () => {
+      const name = 'test-name';
+      const data = { key: 'value' };
+      setSessionStorageByName(name, data);
+      const storedData = window.sessionStorage.getItem(name);
+      expect(storedData).toBe(JSON.stringify(data));
+    });
+  
+    it('removewindow.sessionStorageByName should remove data from session storage', () => {
+      const name = 'test-name';
+      const data = { key: 'value' };
+      window.sessionStorage.setItem(name, JSON.stringify(data));
+      removeSessionStorageByName(name);
+      const storedData = window.sessionStorage.getItem(name);
+      expect(storedData).toBeNull();
+    });
+  
+    it('getSessionStorageByName should return null when data is not found', () => {
+      const name = 'test-name';
+      const storedData = getSessionStorageByName(name);
+      expect(storedData).toBeNull();
+    });
+  
+    it('getSessionStorageByName should return stored data when found', () => {
+      const name = 'test-name';
+      const data = { key: 'value' };
+      window.sessionStorage.setItem(name, JSON.stringify(data));
+      const storedData = getSessionStorageByName(name);
+      expect(storedData).toEqual(data);
+    });
+  });
